871a293b93619e324fa4bdeb2923728c5adb4b92
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2022-03-28 15:27:28 +0100

    Add single post template

--- single.php
+++ single.php
@@ -5,14 +5,11 @@
         while (have_posts()) {
             the_post(); ?>
             <article>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h2><?php the_title(); ?></h2>
                 <div>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(','); ?></div>
-                <?php the_excerpt() ?>
-				<p><a href="<?php the_permalink() ?>">Continue reading &raquo;</a></p>
+                <?php the_content() ?>
             </article>
         <?php }
-
-        echo paginate_links();
     ?>
 </main>
 

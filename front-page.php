b46222cd7f34b52222517d7304efe28d623d7753
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2022-06-16 11:18:33 +0100

    Layout change for homepage

--- front-page.php
+++ front-page.php
@@ -4,9 +4,11 @@
     <?php 
         while (have_posts()) {
             the_post(); ?>
-            <article>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="itangloBlogPostMeta">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(','); ?></div>
+            <article class="itangloBlogPost">
+                <header>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="itangloBlogPostMeta">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(','); ?></div>
+                </header>
                 
                 <?php if ( has_post_thumbnail() ) { ?>
                     <figure class="itangloBlogPostThumbnail">
@@ -14,9 +16,7 @@
                         <figcaption><?php echo get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true ); ?></figcaption>
                     </figure>
                 <?php }
-                
                 the_excerpt() ?>
-				
                 <p><a href="<?php the_permalink() ?>">Continue reading &raquo;</a></p>
             </article>
         <?php }

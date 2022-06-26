b758eabdaa9ebdd4eef075e15aa1cb57ebf2f7f5
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2022-06-26 09:42:17 +0100

    Add post class to single post

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
     <?php 
         while (have_posts()) {
             the_post(); ?>
-            <article>
+            <article class="post">
                 <h2><?php the_title(); ?></h2>
                 <div>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(','); ?></div>
                 <?php the_content() ?>

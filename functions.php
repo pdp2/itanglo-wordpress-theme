cf011dc9ca01bfaef32201ec4265bb6a4897e9ac
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2022-06-26 16:08:47 +0100

    Enqueue main style sheet in functions:
    Means we can add version to CSS files and helps bust cache.

--- functions.php
+++ functions.php
@@ -9,4 +9,9 @@ add_action('wp_enqueue_scripts', 'itanglo_scripts');
 
 function itanglo_scripts() {
     wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,600;0,700;1,400');
+
+    wp_enqueue_style( 'itanglo_styles', get_stylesheet_directory_uri() . '/main.css' ,
+        array(), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
 }

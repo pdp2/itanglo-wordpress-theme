a0add09a919477171adff0305a999749be6e9f56
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2022-06-28 12:59:41 +0100

    Remove post meta from page template

--- page.php
+++ page.php
@@ -6,7 +6,6 @@
             the_post(); ?>
             <article class="post">
                 <h2><?php the_title(); ?></h2>
-                <div>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(','); ?></div>
                 <?php the_content() ?>
             </article>
         <?php }
